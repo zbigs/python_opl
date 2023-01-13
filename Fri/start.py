3228fb03423bbdf9adea3a71e823a3d0b55af54b
zbychs <zbig_s@o2.pl>
2023-01-13 11:56:22 +0100

    a

--- Fri/start.py
+++ Fri/start.py
@@ -2,5 +2,6 @@ import xml.etree.ElementTree as ET
 
 tree = ET.parse("dane.xml")
 root = tree.getroot()
-root.find("./genre/decade/movie[@title='Reservoir Dogs']/year").text = '2023'
+title = 'Reservoir Dogs'
+root.find("./genre/decade/movie[@title='"+title+"']/year").text = '2023'
 tree.write("output.xml")
