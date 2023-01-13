b8b6500728abf95a08ce03459209d568d8a1be0e
zbychs <zbig_s@o2.pl>
2023-01-13 11:48:34 +0100

    a

--- Fri/start.py
+++ Fri/start.py
@@ -1,1 +1,6 @@
-import json
+import xml.etree.ElementTree as ET
+
+tree = ET.parse("dane.xml")
+root = tree.getroot()
+root.find("./genre/decade/movie[@title='Reservoir Dogs']/year").text = '2023'
+tree.write("output.xml")
